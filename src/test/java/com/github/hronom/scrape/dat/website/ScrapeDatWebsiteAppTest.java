d6d93edf87128a365b5da22c103c5f4004c69fbe
Hronom <hronom@gmail.com>
2015-11-14 22:54:08 +0200

    Try fix codecov 4

---
+++ src/test/java/com/github/hronom/scrape/dat/website/ScrapeDatWebsiteAppTest.java
@@ -0,0 +1,25 @@
+package com.github.hronom.scrape.dat.website;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScrapeDatWebsiteAppTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
