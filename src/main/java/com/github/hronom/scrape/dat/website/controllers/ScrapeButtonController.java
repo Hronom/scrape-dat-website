8b18ca3eaea75b1a0a92fe9cbe1f95f9dd62cb4e
Hronom <hronom@gmail.com>
2015-11-14 20:17:51 +0200

    Add unescaping of HTML

--- src/main/java/com/github/hronom/scrape/dat/website/controllers/ScrapeButtonController.java
+++ src/main/java/com/github/hronom/scrape/dat/website/controllers/ScrapeButtonController.java
@@ -7,6 +7,7 @@ import com.gargoylesoftware.htmlunit.WebRequest;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.github.hronom.scrape.dat.website.views.ScrapeView;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
@@ -36,6 +37,8 @@ public class ScrapeButtonController {
         webClient.getOptions().setJavaScriptEnabled(true);
         webClient.getOptions().setPopupBlockerEnabled(false);
         webClient.getOptions().setRedirectEnabled(true);
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
         webClient.setAjaxController(new AjaxController() {
             @Override
             public boolean processSynchron(HtmlPage page, WebRequest request, boolean async) {
@@ -61,6 +64,9 @@ public class ScrapeButtonController {
                             HtmlPage page = webClient.getPage(url);
 
                             String xml = page.asXml();
+
+                            // Unescape html.
+                            xml = StringEscapeUtils.unescapeHtml4(xml);
 
                             String selector = scrapeView.getSelector();
                             if (!xml.isEmpty() && !selector.isEmpty()) {
