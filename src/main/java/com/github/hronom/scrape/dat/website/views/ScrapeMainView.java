69cd5bdfec6d12a5a6fb02690206c879d16eb800
Hronom <hronom@gmail.com>
2015-11-14 17:38:31 +0200

    Add icons

--- src/main/java/com/github/hronom/scrape/dat/website/views/ScrapeMainView.java
+++ src/main/java/com/github/hronom/scrape/dat/website/views/ScrapeMainView.java
@@ -1,6 +1,8 @@
 package com.github.hronom.scrape.dat.website.views;
 
 import java.awt.*;
+import java.net.URL;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
@@ -23,12 +25,27 @@ public class ScrapeMainView {
         constraint.fill = GridBagConstraints.BOTH;
         mainPanel.add(scrapeView, constraint);
 
+        ArrayList<Image> images = new ArrayList<>();
+        images.add(getImage("1447536874_Application.png"));
+        images.add(getImage("1447536961_Application.png"));
+        images.add(getImage("1447536965_Application.png"));
+        images.add(getImage("1447536969_Application.png"));
+        images.add(getImage("1447536973_Application.png"));
+        images.add(getImage("1447536976_Application.png"));
+
         JFrame frame = new JFrame("Scrape dat website!!!");
+        frame.setIconImages(images);
         frame.setContentPane(mainPanel);
         frame.setPreferredSize(new Dimension(800, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    private Image getImage(String fileName) {
+        URL url = this.getClass().getResource(fileName);
+        ImageIcon imageIcon = new ImageIcon(url);
+        return imageIcon.getImage();
     }
 }
